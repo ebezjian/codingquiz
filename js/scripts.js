de1b7b56d62ffd55056cacda5291809f7e296ff0
Ellie Lambert <ebezjian@gmail.com>
2020-10-23 15:57:03 -0400

    debug parseInt in scripts js.

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,24 @@
 $(document).ready(function() {
   $("form#quizs").submit(function(event) {
     event.preventDefault()
-    const colors = parsInt$("input:radio[name=colors]:checked").val();
-    const candies = parsInt$("input:radio[name=candies]:checked").val();
-    const parks = parsInt$("input:radio[name=parks]:checked").val();
-    const location = parsInt$("input:radio[name=location]:checked").val();
-    const animals = parsInt$("input:radio[name=animals]:checked").val();
-    
+    const color = parseInt($("input:radio[name=color]:checked").val());
+    const candies = parseInt($("input:radio[name=candies]:checked").val());
+    const parks = parseInt($("input:radio[name=parks]:checked").val());
+    const location = parseInt($("input:radio[name=location]:checked").val());
+    const animals = parseInt($("input:radio[name=animals]:checked").val());
+    const result = candies + parks + location + animals;
 
     //else and ifs
 
-  if (colors === 5) {
-    
+  if (color === 5 && result >= 20) {
     $("#htmls").show();
     $("#javascript, #csharp").hide();
 
-  } else if (colors === 10) {
-
+  } else if (color === 10 && result >= 20) {
     $("#csharp").show();
     $("#javascript, #html").hide();
 
-  } else (colors ==== 15)  
-    
+  } else (color === 15 && result >= 20)  
     $("#javascript").show();
     $("#csharp, #html").hide();
 
