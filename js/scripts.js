d79f51f746077df110bd0f2455c24c18e2ca190e
Ellie Lambert <ebezjian@gmail.com>
2020-10-23 16:48:08 -0400

    debug javas showing up in styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,28 @@
 
 $(document).ready(function() {
   $("form#quizs").submit(function(event) {
-      event.preventDefault()
+
     const color = parseInt($("select#color").val());
     const candies = parseInt($("candies").val());
     const parks = parseInt($("parks").val());
     const location = parseInt($("locations").val());
     const animals = parseInt($("locations").val());
-    const result = candies + parks + location + animals;
+    total = candies + parks + location + animals;
 
     //else and ifs
 
-  if (color === 5 && result >= 20) {
+  if (color === 1 && total > 20) {
     $("#htmls").show();
-    $("#javascript, #csharp").hide();
+    $("#javas, #csharp").hide();
 
-  } else if (color === 10 && result >= 20) {
+  } else if (color === 2 && total > 20) {
     $("#csharp").show();
-    $("#javascript, #htmls").hide();
+    $("#javas, #htmls").hide();
 
-  } else (color === 15 && result >= 20);
+  } else if (color === 3 && total > 20){
     $("#javascript").show();
-    $("#csharp, #htmls").hide();
-  });
-});
+    $("#csharp, #htmls").hide(); 
+    event.preventDefault() 
+  };
+  
+  })})
